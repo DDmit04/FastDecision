d7905f563a8341c868b1c659378ad377492c4023
DDmit04 <danya.dmit1@gmail.com>
2019-12-09 19:56:38 +0300

    fix 404 server redirect(2)?

--- src/main/java/hackaton/fastdisision/controller/MainController.java
+++ src/main/java/hackaton/fastdisision/controller/MainController.java
@@ -4,14 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Controller
+@RequestMapping("/")
 public class MainController {
 
     @Value("${spring.profiles.active:prod}")
     private String profile;
 
-    @GetMapping("/")
+    @GetMapping
     public String main(Model model) {
         boolean isDevMode = true;
         if(profile.equals("prod")) {
