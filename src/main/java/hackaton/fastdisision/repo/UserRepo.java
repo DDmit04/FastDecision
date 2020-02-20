7bd947cd9cd2381ce554bd562219d77996adb4d0
DDmit04 <danya.dmit1@gmail.com>
2020-02-20 22:21:35 +0300

    deploy fix

--- src/main/java/hackaton/fastdisision/repo/UserRepo.java
+++ src/main/java/hackaton/fastdisision/repo/UserRepo.java
@@ -7,8 +7,6 @@ import java.util.Optional;
 
 public interface UserRepo extends CrudRepository<User, String> {
 
-    User findByUsername(String username);
-
     Optional<User> findById(String Id);
 
 }
--- src/main/resources/db/migration/V4__add_user_roles.sql
+++ src/main/resources/db/migration/V4__add_user_roles.sql
@@ -1,10 +1,1 @@
 alter table usr add column password varchar(255) not null;
-
-create table user_role
-(
-    user_id varchar(255) not null,
-    roles   varchar(255)
-);
-
-alter table if exists user_role
-    add constraint FKfpm8swft53ulq2hl11yplpr5 foreign key (user_id) references usr;
