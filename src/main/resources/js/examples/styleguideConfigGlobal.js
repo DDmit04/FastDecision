f4b8b6dbade09bee7ae1cd47f8c892442af806e1
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2020-04-01 02:33:13 +0300

    fix static docs

--- src/main/resources/js/examples/styleguideConfigGlobal.js
+++ src/main/resources/js/examples/styleguideConfigGlobal.js
@@ -3,6 +3,7 @@ import vuetifyPlugin from "../../plugins/vuetifyPlugin";
 import 'vuetify/dist/vuetify.min.css'
 import Donut from 'vue-css-donut-chart'
 import filters from "../filters/filters"
+import 'regenerator-runtime'
 
 for(let name in filters) {
     Vue.filter(name, filters[name]);
